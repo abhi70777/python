858159b807d155d6a4ba0d83356e0d20b36983db
Abhinand V.B <7abhinand7@gmail.com>
2025-10-15 13:45:33 +0530

    task

---
+++ day1classwork.py
@@ -0,0 +1,26 @@
+a = 15.5
+b = 20
+c = 10.25
+
+total=a+b+c
+
+print(total)
+
+ty=int(total)
+
+print(ty)
+
+tt=str(total)
+
+print(tt)
+
+
+import random
+
+bonus=(random.randrange(5, 10))
+final=total+bonus
+
+print(final)
+
+
+
---
+++ day1homework.py
@@ -0,0 +1,27 @@
+r,s,o = 45,40,130
+
+q,f,g = 3,2.5,1.8
+
+total_rice = r*q
+total_sugar = s*f
+total_oil = o*g
+
+print(total_rice,total_sugar,total_oil)
+
+final=(total_rice+total_sugar+total_oil)
+
+print(final)
+
+i=int(final)
+
+s=str(final)
+
+print(i)
+
+print(s)
+
+import random
+
+delivery=(random.randrange (5,10))
+
+print(final+delivery)
